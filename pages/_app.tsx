e813985fdb17876c9ad62acdbf119cc16b2ad322
Abdul Ahad Siddiqui <abdulahadsiddiqui011@gmail.com>
2022-12-11 23:50:02 +0530

    Add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { Layout } from '../components';
 import type { AppProps } from 'next/app'
 
+import { GoogleAnalytics } from "nextjs-google-analytics";
+
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <GoogleAnalytics trackPageViews gaMeasurementId='G-3KHE49GZBF' />
       <Component {...pageProps} />
     </Layout>
   )
